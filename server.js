2bab5b402bdfd2ef26c3d4198600bc944df8d45a
Nattawat Isanpayu <nattawat@MacBook-Pro-khxng-Nattawat.local>
2024-02-05 09:18:41 +0700

    add cors

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const requestRoutes = require('./src/requests/routes')
 const userRoutes = require('./src/users/routes')
 const app = express()
 const port = 3000
+const cors = require('cors');
 
 app.use(express.json())
+app.use(cors());
+
 
 app.get("/", (req,res) => {
     res.send('hello world')
